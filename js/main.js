907b8456c0126aedd9806d7bd0e4a88f600688d4
babak zarrinbal <babak.zarrinbal@gmail.com>
2019-10-07 15:11:24 +0330

    voice changing

--- js/main.js
+++ js/main.js
@@ -1,93 +1,107 @@
 window.onload = () => {
-  'use strict';
+  "use strict";
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
   }
-}
+};
 
 var playingta = document.getElementById("playing");
-    var playedta = document.getElementById("played");
-    var playingtext;
-    var toplaytext;
-    var rate;
-    var stoppedplayback = false;
+var playedta = document.getElementById("played");
+var playingtext;
+var toplaytext;
+var rate;
+var stoppedplayback = false;
+var selectedvoice;
+var play = function(frombegining) {
+  if (stoppedplayback) {
+    speechSynthesis.cancel();
+    stoppedplayback = false;
+    return;
+  }
+  playingta = document.getElementById("playing");
+  playedta = document.getElementById("played");
+  if (!playingta.value.length) return;
+  var msg = new SpeechSynthesisUtterance();
+  rate = document.getElementById("speakrate").value;
 
-    var play = function(frombegining) {
-      if (stoppedplayback) {
-        speechSynthesis.cancel();
-        stoppedplayback = false;
-        return;
-      }
-      playingta = document.getElementById("playing");
-      playedta = document.getElementById("played");
-      if (!playingta.value.length) return;
-      var msg = new SpeechSynthesisUtterance();
-      rate = document.getElementById("speakrate").value;
+  let firstsentence = Math.min(
+    playingta.value.indexOf("."),
+    playingta.value.indexOf("\n")
+  );
 
-      let firstsentence = Math.min(
-        playingta.value.indexOf("."),
-        playingta.value.indexOf("\n")
-      );
-
-      firstsentence =
-        firstsentence == -1 ? playingta.value.length : firstsentence + 1;
-      msg.text = playingta.value.slice(0, firstsentence);
-      msg.lang='en-US';
-      msg.rate = rate;
-      msg.onend = function(ev) {
-        if (!stoppedplayback) {
-          playedta.value += playingta.value.slice(0, firstsentence);
-          playingta.value = playingta.value.slice(firstsentence);
-        }
-        return play();
-      };
-      speechSynthesis.speak(msg);
-    };
-    var loadprev = function() {
-      let ev = document.getElementById("storyval").value;
-      playingta.value = window.localStorage.getItem("playingta_" + ev);
-      playedta.value = window.localStorage.getItem("playedta_" + ev);
-    };
-    var savecur = function(hidden) {
-      let ev = document.getElementById("storyval").value;
-      window.localStorage.setItem("playingta_" + ev, playingta.value);
-      window.localStorage.setItem("playedta_" + ev, playedta.value);
-      !hidden && alert("saved");
-    };
-    var clearall = function() {
-      playingta.value = "";
-      playedta.value = "";
-    };
-    var reset = function() {
-      if (!confirm("sure to reset this text?")) return;
-      let ev = document.getElementById("storyval").value;
-      playingta.value = (stories[ev] || "").toString();
-      playedta.value = "";
-      window.localStorage.setItem("playingta_" + ev, "");
-      window.localStorage.setItem("playedta_" + ev, "");
-    };
-    savedstories = function(ev) {
-      playingta.value = window.localStorage.getItem("playingta_" + ev) || "";
-      playedta.value = window.localStorage.getItem("playedta_" + ev) || "";
-      if (!playingta.value.length) {
-        playingta.value = (stories[ev] || "").toString();
-        playedta.value = "";
-        window.localStorage.setItem("playingta_" + ev, playingta.value);
-        window.localStorage.setItem("playedta_" + ev, "");
-      }
-    };
-    var stories = [];
-    for (let i = 0; i < 18; i++) {
-      window
-        .fetch("resource/stories/" + i + ".txt")
-        .then(res => res.text())
-        .then(r => (stories[i] = r));
+  firstsentence =
+    firstsentence == -1 ? playingta.value.length : firstsentence + 1;
+  msg.text = playingta.value.slice(0, firstsentence);
+  if (selectedvoice) msg.voice = selectedvoice;
+  msg.rate = rate;
+  msg.onend = function(ev) {
+    if (!stoppedplayback) {
+      playedta.value += playingta.value.slice(0, firstsentence);
+      playingta.value = playingta.value.slice(firstsentence);
     }
+    return play();
+  };
+  speechSynthesis.speak(msg);
+};
+var loadprev = function() {
+  let ev = document.getElementById("storyval").value;
+  playingta.value = window.localStorage.getItem("playingta_" + ev);
+  playedta.value = window.localStorage.getItem("playedta_" + ev);
+};
+var savecur = function(hidden) {
+  let ev = document.getElementById("storyval").value;
+  window.localStorage.setItem("playingta_" + ev, playingta.value);
+  window.localStorage.setItem("playedta_" + ev, playedta.value);
+  !hidden && alert("saved");
+};
+var clearall = function() {
+  playingta.value = "";
+  playedta.value = "";
+};
+var reset = function() {
+  if (!confirm("sure to reset this text?")) return;
+  let ev = document.getElementById("storyval").value;
+  playingta.value = (stories[ev] || "").toString();
+  playedta.value = "";
+  window.localStorage.setItem("playingta_" + ev, "");
+  window.localStorage.setItem("playedta_" + ev, "");
+};
+savedstories = function(ev) {
+  playingta.value = window.localStorage.getItem("playingta_" + ev) || "";
+  playedta.value = window.localStorage.getItem("playedta_" + ev) || "";
+  if (!playingta.value.length) {
+    playingta.value = (stories[ev] || "").toString();
+    playedta.value = "";
+    window.localStorage.setItem("playingta_" + ev, playingta.value);
+    window.localStorage.setItem("playedta_" + ev, "");
+  }
+};
+var stories = [];
+for (let i = 0; i < 18; i++) {
+  window
+    .fetch("resource/stories/" + i + ".txt")
+    .then(res => res.text())
+    .then(r => (stories[i] = r));
+}
+var voices = [];
+window.onload = () => {
+  const awaitVoices = new Promise(
+    done => (speechSynthesis.onvoiceschanged = done)
+  );
 
-    window.onload = () => {
-      if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("./sw.js");
-      }
-    };
+  awaitVoices.then(() => {
+    voices = speechSynthesis.getVoices();
+    var sel = document.getElementById("voiceselect");
+    voices.forEach(function(v, i) {
+      var opt = document.createElement("option");
+      opt.appendChild(document.createTextNode(v.name));
+      opt.value = i;
+      sel.appendChild(opt);
+    });
+  });
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
+};
