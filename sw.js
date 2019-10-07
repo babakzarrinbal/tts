4f37193a7fa833a0269d889147459e96c9e0814e
babak zarrinbal <babak.zarrinbal@gmail.com>
2019-10-07 16:38:44 +0330

    removed sw resouce cache

--- sw.js
+++ sw.js
@@ -5,9 +5,7 @@ var filesToCache = [
   '/tts/css/style.css',
   '/tts/js/main.js'
 ];
-for(i=0;i<20;i++){
-  filesToCache.push("/tts/resource/stories/"+i+".txt");
-}
+
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
