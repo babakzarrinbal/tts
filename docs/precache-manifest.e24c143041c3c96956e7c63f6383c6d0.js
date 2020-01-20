d6505a3946bfbda1599f559e29292b4f2c7ce3ee
babak <babak.zarrinbal@gmail.com>
2020-01-20 18:23:33 +0330

    initialization from zmovies

---
+++ docs/precache-manifest.e24c143041c3c96956e7c63f6383c6d0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "85a5675b8b7737090914d26418d8175d",
+    "url": "/tts/img/ttsicons/next-w.svg"
+  },
+  {
+    "revision": "536ef82528266ef7716cc76f13c960e4",
+    "url": "/tts/img/loaders/movie.gif"
+  },
+  {
+    "revision": "a661ba92bda1f828eb4b",
+    "url": "/tts/js/chunk-15663170.26149564.js"
+  },
+  {
+    "revision": "e8ff09c5bc760608e7431f14032fd293",
+    "url": "/tts/logo.png"
+  },
+  {
+    "revision": "171a965fd157d263ce5b",
+    "url": "/tts/js/chunk-vendors.64c5302e.js"
+  },
+  {
+    "revision": "101a8cc034ef8270a3f2",
+    "url": "/tts/js/pdfjsWorker.783a4eb0.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tts/robots.txt"
+  },
+  {
+    "revision": "ff6a57ed80c995ffe9ddd1fb11a71e41",
+    "url": "/tts/index.html"
+  },
+  {
+    "revision": "72d4a6cd86f52d8d64c4eefb089542cb",
+    "url": "/tts/img/ttsicons/shield.svg"
+  },
+  {
+    "revision": "50acadf632bc9506653499fff9bb30ac",
+    "url": "/tts/img/ttsicons/settings.svg"
+  },
+  {
+    "revision": "8181039b61cfc565659fa059c039e706",
+    "url": "/tts/img/ttsicons/plus.svg"
+  },
+  {
+    "revision": "ce554f9aa15080bcf5d30af0549b0779",
+    "url": "/tts/img/ttsicons/refresh.svg"
+  },
+  {
+    "revision": "e3eca74d0d77d7299725c27949c59ebd",
+    "url": "/tts/img/loaders/rings.svg"
+  },
+  {
+    "revision": "d8f8be8601d46e34d280ea4e44a5be1d",
+    "url": "/tts/img/loaders/puff.svg"
+  },
+  {
+    "revision": "783c7836eb439161b1f9fa84430c7bf9",
+    "url": "/tts/img/ttsicons/list.svg"
+  },
+  {
+    "revision": "f7caff4f185c623ba45c02ab832f9c1f",
+    "url": "/tts/img/ttsicons/bookmark.svg"
+  },
+  {
+    "revision": "20ca233e48ad5b5a127f6f1d86a4e4ef",
+    "url": "/tts/img/ttsicons/minus.svg"
+  },
+  {
+    "revision": "b16e073b1de2fdfc6b4f",
+    "url": "/tts/js/app.966e4db9.js"
+  },
+  {
+    "revision": "3a1311d5e71f7d0d9cf75005f1992426",
+    "url": "/tts/img/ttsicons/next.svg"
+  },
+  {
+    "revision": "2d862f5aaa34fed9e533fbfc84d4080e",
+    "url": "/tts/img/ttsicons/pause-w.svg"
+  },
+  {
+    "revision": "6ee7698493609851c684cc8b560084a7",
+    "url": "/tts/img/ttsicons/play-full.svg"
+  },
+  {
+    "revision": "14b4111eac1e3e8aeaaee5e2d9685dc6",
+    "url": "/tts/img/ttsicons/pause.svg"
+  },
+  {
+    "revision": "eb811774ab3b6bec73673e8ec3c24105",
+    "url": "/tts/img/ttsicons/play-w.svg"
+  },
+  {
+    "revision": "7db92e62bee29459989694fb4526bc16",
+    "url": "/tts/img/ttsicons/play.svg"
+  },
+  {
+    "revision": "01711fa464361d1f2c5994e387288432",
+    "url": "/tts/img/icon/notsee.png"
+  },
+  {
+    "revision": "7e7fd524bff2b430af8a924bae7b6fad",
+    "url": "/tts/img/icon/list.png"
+  },
+  {
+    "revision": "39153b06929a746eee884dd665ab0dc8",
+    "url": "/tts/img/icon/setting.png"
+  },
+  {
+    "revision": "3e7f8afa35e80c2ec94e5d93bbf47b3a",
+    "url": "/tts/img/icon/see.png"
+  },
+  {
+    "revision": "da8857c4b5ff5228eda7b8bad5514750",
+    "url": "/tts/img/icon/downloadqueue.png"
+  },
+  {
+    "revision": "8f3082d40ef4c4f2e90c48c4f944d8af",
+    "url": "/tts/db/movies/list/lastupdated/1.json.bak2"
+  },
+  {
+    "revision": "32b7ab1d4cfffb87627042b923affa1a",
+    "url": "/tts/db/movies/list/lastupdated/1.json.bak"
+  },
+  {
+    "revision": "03c0e918cda12bed27680b35d66c1977",
+    "url": "/tts/db/movies/list/lastupdated/1.json"
+  },
+  {
+    "revision": "171a965fd157d263ce5b",
+    "url": "/tts/css/chunk-vendors.6acb9ff2.css"
+  },
+  {
+    "revision": "b16e073b1de2fdfc6b4f",
+    "url": "/tts/css/app.a827ad5a.css"
+  },
+  {
+    "revision": "193b96b34d94edc0b5c13fd6be45597a",
+    "url": "/tts/44e72d1c0f74c3c804e8.worker.js"
+  }
+];
